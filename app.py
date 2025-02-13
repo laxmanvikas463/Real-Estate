9910eeef6574079dc989fb30b1c09b0a9303ea89
laxmanvikas463 <134911491+laxmanvikas463@users.noreply.github.com>
2025-02-13 09:06:01 +0530

    CORS app.py

--- app.py
+++ app.py
@@ -262,8 +262,10 @@ def get_response(msg):
 
 ############
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 @app.route('/')
 def home():
     return jsonify({'message': 'Flask on Cloud Run is live!'})
