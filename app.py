c9f82a8642450ab98a92119ffa2bd7e0a99d2952
laxmanvikas463 <134911491+laxmanvikas463@users.noreply.github.com>
2025-02-13 09:14:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -265,7 +265,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app, resources={r"/*": {"origins": "*"}})  # Enable CORS for all routes
 @app.route('/')
 def home():
     return jsonify({'message': 'Flask on Cloud Run is live!'})
